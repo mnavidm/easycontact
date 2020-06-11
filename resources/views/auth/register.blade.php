51b93df567c8b4559f234411423108b49a9a51bc
Navid Mardukh <mnavidm@gmail.com>
2020-06-11 09:10:41 +0430

    edit some of auth

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -61,6 +61,25 @@
                             </div>
                         </div>
 
+                        <div class="form-group row">
+                            <label for="easyname" class="col-md-4 col-form-label text-md-right">{{ __('Easy name') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="easyname" type="text" class="form-control" name="easyname" required>
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="public" class="col-md-4 col-form-label text-md-right">{{ __('Public') }}</label>
+
+                            <div class="col-md-6">
+                                <input type="radio" id="public" name="public" value="1">
+                                <label for="male">Public</label><br>
+                                <input type="radio" id="private" name="public" value="0" checked="checked" >
+                                <label for="female" >Private</label><br>
+                            </div>
+                        </div>
+
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
